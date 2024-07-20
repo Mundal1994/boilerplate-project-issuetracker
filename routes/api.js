af25243453fba91b72ee6741769f5577772c6bd8
Mundal1994 <manga_flip@hotmail.com>
2024-07-20 18:37:28 +0300

    implemented .post function and tests

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,54 @@ module.exports = function (app) {
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let project = req.params.project;
-      
     })
     
     .post(function (req, res){
       let project = req.params.project;
       
+      if (req.body.issue_title == null || req.body.issue_text == null || req.body.created_by == null) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.json({error: 'required field(s) missing'});
+        return;
+      }
+
+      let id = "";
+      const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+      for (let i = 0; i < 24; i++) {
+        id += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+
+      const currentDate = new Date().toISOString();
+      const result = {
+        "_id": id,
+        "issue_title": req.body.issue_title,
+        "issue_text": req.body.issue_text,
+        "created_on": currentDate,
+        "updated_on": currentDate,
+        "created_by": req.body.created_by,
+        "assigned_to": req.body.assigned_to == null ? '' : req.body.assigned_to,
+        "open": true,
+        "status_text": req.body.status_text == null ? '' : req.body.status_text
+      };
+      console.log("result: ", result);
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.json(result);
     })
     
     .put(function (req, res){
       let project = req.params.project;
+
+      const currentDate = new Date().toISOString();
       
+      // with an _id and one or more fields to update. On success, the updated_on field should be updated, and returned should be {  result: 'successfully updated', '_id': _id }.
+      // error handling: does not include an _id, the return value is { error: 'missing _id' }.
+      // does not include update fields, the return value is { error: 'no update field(s) sent', '_id': _id }. On any other error, the return value is { error: 'could not update', '_id': _id }.
     })
     
     .delete(function (req, res){
       let project = req.params.project;
+
+      //You can send a DELETE request to /api/issues/{projectname} with an _id to delete an issue. If no _id is sent, the return value is { error: 'missing _id' }. On success, the return value is { result: 'successfully deleted', '_id': _id }. On failure, the return value is { error: 'could not delete', '_id': _id }.
       
     });
     
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,106 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  suite('POST /api/issues/:project calls', function () {
+    test('Test POST /api/issues/apitest every field', function(done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .post('/api/issues/apitest')
+        .send({ 
+            "issue_title": "Fix error in posting data",
+            "issue_text": "When we post data it has an error.",
+            "created_by": "Joe",
+            "assigned_to": "Joe",
+            "status_text": "In QA"
+          })
+        .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.issue_title, 'Fix error in posting data');
+            assert.equal(res.body.issue_text, 'When we post data it has an error.');
+            assert.equal(res.body.created_by, 'Joe');
+            assert.equal(res.body.assigned_to, 'Joe');
+            assert.equal(res.body.status_text, 'In QA');
+            done();
+        })
+    });
+    test('Test POST /api/issues/apitest only required fields', function(done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .post('/api/issues/apitest')
+          .send({ 
+              "issue_title": "Fix error in posting data",
+              "issue_text": "When we post data it has an error.",
+              "created_by": "Joe"
+            })
+          .end(function(err, res) {
+              assert.equal(res.status, 200);
+              assert.equal(res.body.issue_title, 'Fix error in posting data');
+              assert.equal(res.body.issue_text, 'When we post data it has an error.');
+              assert.equal(res.body.created_by, 'Joe');
+              assert.equal(res.body.assigned_to, '');
+              assert.equal(res.body.status_text, '');
+              done();
+          })
+      });
+      test('Test POST /api/issues/apitest missing required fields', function(done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .post('/api/issues/apitest')
+          .send({ 
+            "assigned_to": "Joe",
+            "status_text": "In QA"
+            })
+          .end(function(err, res) {
+              assert.equal(res.status, 200);
+              assert.equal(res.body.error, 'required field(s) missing');
+              done();
+          })
+      });
+  });
+  const currentDatabase = [{
+    '_id': 'bn0zdgznxzf7ozxxcfcvsbwa',
+    'issue_title': 'Fix error in posting data',
+    'issue_text': 'When we post data it has an error.',
+    'created_on': '2024-07-20T15:21:11.838Z',
+    'updated_on': '2024-07-20T15:21:11.838Z',
+    'created_by': 'Joe',
+    'assigned_to': 'Joe',
+    'open': true,
+    'status_text': 'In QA'
+  },
+  {
+    '_id': 'q2zlh4076etyr5wezqtupb76',
+    'issue_title': 'Fix error in posting data',
+    'issue_text': 'When we post data it has an error.',
+    'created_on': '2024-07-20T15:21:11.865Z',
+    'updated_on': '2024-07-20T15:21:11.865Z',
+    'created_by': 'Joe',
+    'assigned_to': '',
+    'open': true,
+    'status_text': ''
+  },
+  {
+    '_id': 'q2zlh4076etyr5we25tupb76',
+    'issue_title': 'Guitar issue',
+    'issue_text': 'Issues of missing guitar stand.',
+    'created_on': '2024-07-20T15:22:11.865Z',
+    'updated_on': '2024-07-20T15:22:11.865Z',
+    'created_by': 'Ronan',
+    'assigned_to': 'Me',
+    'open': true,
+    'status_text': ''
+  }];
+  suite('GET /api/issues/:project calls', function () {
+    
+  });
+  suite('PUT /api/issues/:project calls', function () {
+    //.put('/travellers) 
+    //.send({"surname": "Colombo"})
+  });
+  suite('DELETE /api/issues/:project calls', function () {
+  });
+  // at least 14 tests has to be made. 4 tests in each suit
 });
